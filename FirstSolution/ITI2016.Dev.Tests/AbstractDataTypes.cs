7e4f65a270303923d0d0d3ce3d595085fcacfc90
Olivier Spinelli <olivier.spinelli@invenietis.com>
2016-03-17 17:38:33 +0100

    Abstract Data Types.

--- FirstSolution/ITI2016.Dev.Tests/AbstractDataTypes.cs
+++ FirstSolution/ITI2016.Dev.Tests/AbstractDataTypes.cs
@@ -6,7 +6,59 @@ using System.Threading.Tasks;
 
 namespace ITI2016.Dev.Tests
 {
-    class AbstractDataTypes
+
+    interface IEnumerable<T>
     {
+        IEnumerator<T> GetEnumerator();
     }
+
+    interface IReadOnlyCollection<T>
+    {
+        int Count { get; }
+
+        IEnumerator<T> GetEnumerator();
+    }
+
+    interface IReadOnlyList<T> : IReadOnlyCollection<T>
+    {
+        T this[int i] { get; }
+    }
+
+    interface IList<T> : IReadOnlyList<T>
+    {
+        new T this[int i] { get; set; }
+
+        void Add( T e );
+
+        void InsertAt( int i, T e );
+
+        void RemoveAt( int i );
+    }
+
+    interface ISet<T> : IReadOnlyCollection<T>
+    {
+        void Add( T e );
+
+        bool Contains( T e );
+
+        void Remove( T e );
+    }
+
+    interface IEnumerator<T>
+    {
+        bool MoveNext();
+
+        T Current { get; }
+    }
+
+    interface IEnumeratorJava<T>
+    {
+        bool HasNext();
+
+        T GetNext();
+
+    }
+
+
+
 }
