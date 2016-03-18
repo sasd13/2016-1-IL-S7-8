0e412d521467978a531c7482bc7ad6cd87ef495e
Olivier Spinelli <olivier.spinelli@invenietis.com>
2016-03-18 10:21:25 +0100

    Starting implementation.

--- FirstSolution/ITI2016.Dev/List.cs
+++ FirstSolution/ITI2016.Dev/List.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,38 +9,49 @@ namespace ITI2016.Dev
 {
     public class List<T> : IList<T>
     {
+        T[] _array;
+        int _count;
+
+        public List()
+        {
+            _array = new T[4];
+        }
+
+
         public T this[int i]
         {
             get
             {
-                throw new NotImplementedException();
+                if( i < 0 || i >= _count ) throw new InvalidOperationException();
+                return _array[i];
             }
 
             set
             {
-                throw new NotImplementedException();
+                if( i < 0 || i >= _count ) throw new InvalidOperationException();
+                _array[i] = value;
             }
         }
 
-        T IReadOnlyList<T>.this[int i]
-        {
-            get
-            {
-                throw new NotImplementedException();
-            }
-        }
-
-        public int Count
-        {
-            get
-            {
-                throw new NotImplementedException();
-            }
-        }
+        public int Count => _count;
 
         public void Add( T e )
         {
-            throw new NotImplementedException();
+            if( _count < _array.Length )
+            {
+                _array[_count++] = e;
+            }
+            else
+            {
+                Debug.Assert( _count == _array.Length, "This is obvious!" );
+                T[] t = new T[_count + 1];
+                for( int i = 0; i < _count; ++i )
+                {
+                    t[i] = _array[i];
+                }
+                t[_count++] = e;
+                _array = t;
+            }
         }
 
         public void Clear()
