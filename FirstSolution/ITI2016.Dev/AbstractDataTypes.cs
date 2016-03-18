c07360fd7f0838278c0a1c2c5a915b93b334cf1e
Olivier Spinelli <olivier.spinelli@invenietis.com>
2016-03-18 17:55:50 +0100

    Next step: the dictionary.

--- FirstSolution/ITI2016.Dev/AbstractDataTypes.cs
+++ FirstSolution/ITI2016.Dev/AbstractDataTypes.cs
@@ -44,6 +44,60 @@ namespace ITI2016.Dev
         void Remove( T e );
     }
 
+    public struct KeyValuePair<TKey,TValue>
+    {
+        public readonly TKey Key;
+        public readonly TValue Value;
+
+        public KeyValuePair( TKey key, TValue value )
+        {
+            Key = key;
+            Value = value;
+        }
+    }
+
+    public interface IDictionary<TKey, TValue> : IReadOnlyCollection<KeyValuePair<TKey,TValue>>
+    {
+        IEnumerable<TKey> Keys { get; }
+
+        IEnumerable<TValue> Values { get; }
+
+        bool ContainsKey( TKey k );
+
+        /// <summary>
+        /// Checks whether this dictionary contains the given value.
+        /// Caution: this is an O(n) operation!
+        /// </summary>
+        /// <param name="v">The value to lookup.</param>
+        /// <returns>Tru when found, false otherwise.</returns>
+        bool ContainsValue( TValue v );
+
+        /// <summary>
+        /// Removes a key/value pair.
+        /// </summary>
+        /// <param name="key">The key to remove.</param>
+        /// <returns>True if the key has been found and removed, false otherwise.</returns>
+        bool Remove( TKey key );
+
+        /// <summary>
+        /// Gets or sets the value associated to the given key.
+        /// When getting, the key MUST exist otherwise a <see cref="KeyNotFoundException"/> is 
+        /// thrown.
+        /// </summary>
+        /// <param name="k">The key.</param>
+        /// <returns>The associated value.</returns>
+        TValue this[TKey k] { get; set; }
+
+        /// <summary>
+        /// Adds a key/value pair. The key MUST NOT exist otherwise an exception is thrown.
+        /// </summary>
+        /// <param name="k">The key to add.</param>
+        /// <param name="v">The associated value.</param>
+        void Add( TKey k, TValue v );
+
+
+    }
+
     public interface IEnumerator<T>
     {
         bool MoveNext();
