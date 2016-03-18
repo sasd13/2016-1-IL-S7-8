1699bfa10dd7675f3b288ddd50a213c840d5ac43
Olivier Spinelli <olivier.spinelli@invenietis.com>
2016-03-18 09:33:13 +0100

    Starting List<T> implementation.

---
+++ FirstSolution/ITI2016.Dev/List.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ITI2016.Dev
+{
+    public class List<T> : IList<T>
+    {
+        public T this[int i]
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+
+            set
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        T IReadOnlyList<T>.this[int i]
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public int Count
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public void Add( T e )
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Clear()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void InsertAt( int i, T e )
+        {
+            throw new NotImplementedException();
+        }
+
+        public void RemoveAt( int i )
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
