289c9dbf7669254fce70ddbc8874c229ecad1372
Olivier Spinelli <olivier.spinelli@invenietis.com>
2016-03-25 10:18:58 +0100

    Preparing Dictionary implementation.

---
+++ FirstSolution/ITI2016.Dev/Dictionary.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ITI2016.Dev
+{
+    public class Dictionary<TKey, TValue> : IDictionary<TKey, TValue>
+    {
+        class Node
+        {
+            public readonly TKey Key;
+            public TValue Value;
+            public Node Next;
+            public Node( TKey k )
+            {
+                Key = k;
+            }
+        }
+        Node[] _buckets;
+        int _count;
+
+        public Dictionary()
+        {
+            _buckets = new Node[7];
+        }
+
+        public TValue this[TKey k]
+        {
+            get
+            {
+                // 1 - Finds the bucket index.
+                // 2 - Finds the Node in the linked list where node.Key equals k.
+                // 3 - if node is found returns node.Value otherwise throws a KeyNotFoundException.
+                throw new NotImplementedException();
+            }
+
+            set
+            {
+                // 1 - Finds the bucket index.
+                // 2 - Finds the Node in the linked list where node.Key equals k.
+                // 3 - if node found, updates node.Value to value.
+                //     Otherwise inserts a new Node in the linked list with k and v.
+                throw new NotImplementedException();
+            }
+        }
+
+        public int Count => _count;
+
+        public IEnumerable<TKey> Keys
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public IEnumerable<TValue> Values
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public void Add( TKey k, TValue v )
+        {
+            // 1 - Finds the bucket index.
+            // 2 - Ensures that k does not appear in the linked list of node for the bucket.
+            //     If a node is found throws an InvalidArgumentException.
+            // 3 - Inserts a new Node in the linked list with k and v.
+        }
+
+        public bool ContainsKey( TKey k )
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool ContainsValue( TValue v )
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerator<KeyValuePair<TKey, TValue>> GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Remove( TKey key )
+        {
+            // 1 - Finds the bucket index.
+            // 2 - Finds the Node in the linked list where node.Key equals k.
+            // 3 - If node found, removes it and returns true.
+            //     Otherwise returns false.
+            throw new NotImplementedException();
+        }
+    }
+}
