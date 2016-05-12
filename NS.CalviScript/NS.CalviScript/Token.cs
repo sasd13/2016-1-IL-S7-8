c0906eec09fcc8e591403f147625ef711a73fb23
Antoine Raquillet <antoine.raquillet@invenietis.com>
2016-05-12 16:22:50 +0200

    First (ugly) tokenizer.

---
+++ NS.CalviScript/NS.CalviScript/Token.cs
@@ -0,0 +1,20 @@
+﻿namespace NS.CalviScript
+{
+    public class Token
+    {
+        public Token( TokenType type )
+            : this( type, string.Empty )
+        {
+        }
+
+        public Token( TokenType type, string value )
+        {
+            Type = type;
+            Value = value;
+        }
+
+        public TokenType Type { get; }
+
+        public string Value { get; }
+    }
+}
---
+++ NS.CalviScript/NS.CalviScript/Tokenizer.cs
@@ -0,0 +1,43 @@
+﻿using System.Diagnostics;
+using System.Text;
+
+namespace NS.CalviScript
+{
+    public class Tokenizer
+    {
+        readonly string _input;
+        int _pos;
+
+        public Tokenizer( string input )
+        {
+            _input = input;
+        }
+
+        public Token GetNextToken()
+        {
+            char current;
+            while( char.IsWhiteSpace( current = _input[ _pos ] ) ) _pos++;
+            Token result;
+            if( current == '+' ) result = new Token( TokenType.Plus );
+            else if( current == '(' ) result = new Token( TokenType.LeftParenthesis );
+            else if( current == ')' ) result = new Token( TokenType.RightParenthesis );
+            else if( char.IsDigit( current ) ) return GetNextNumberToken();
+            else result = new Token( TokenType.None );
+            _pos++;
+            return result;
+        }
+
+        Token GetNextNumberToken()
+        {
+            Debug.Assert( char.IsDigit( _input[ _pos ] ) );
+
+            StringBuilder sb = new StringBuilder();
+            do
+            {
+                sb.Append( _input[ _pos ] );
+                _pos++;
+            } while( char.IsDigit( _input[ _pos ] ) );
+            return new Token( TokenType.Number, sb.ToString() );
+        }
+    }
+}
