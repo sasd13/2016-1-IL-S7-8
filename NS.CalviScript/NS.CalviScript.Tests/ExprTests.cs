81e427fe48e7490bbb6b8f08a332681070e0c529
Antoine Raquillet <antoine.raquillet@invenietis.com>
2016-05-23 18:01:42 +0200

    Added visitors.

---
+++ NS.CalviScript/NS.CalviScript.Tests/ExprTests.cs
@@ -0,0 +1,27 @@
+﻿using NUnit.Framework;
+
+namespace NS.CalviScript.Tests
+{
+    [TestFixture]
+    public class ExprTests
+    {
+        [Test]
+        public void can_stringify()
+        {
+            IExpr expr = new BinaryExpr(
+                TokenType.Plus,
+                new BinaryExpr(
+                    TokenType.Plus,
+                    new ConstantExpr( 2 ),
+                    new ConstantExpr( 7 ) ),
+                new BinaryExpr(
+                    TokenType.Mult,
+                    new ConstantExpr( 5 ),
+                    new ConstantExpr( 8 ) ) );
+
+            string result = expr.ToInfixString();
+
+            Assert.That( result, Is.EqualTo( "((2 + 7) + (5 * 8))" ) );
+        }
+    }
+}
