5cb2293082f74b37a5fc6eb015e6e6922c6021f8
Antoine Raquillet <antoine.raquillet@invenietis.com>
2016-05-24 10:10:16 +0200

    Added eval visitor implementation.

--- NS.CalviScript/NS.CalviScript/Visitors/EvalVisitor.cs
+++ NS.CalviScript/NS.CalviScript/Visitors/EvalVisitor.cs
@@ -10,17 +10,40 @@ namespace NS.CalviScript
     {
         public void Visit( ErrorExpr expr )
         {
-            throw new NotImplementedException();
+            throw new InvalidOperationException( expr.Message );
         }
 
         public void Visit( ConstantExpr expr )
         {
-            throw new NotImplementedException();
+            Result = expr.Value;
         }
 
         public void Visit( BinaryExpr expr )
         {
-            throw new NotImplementedException();
+            expr.LeftExpr.Accept( this );
+            int left = Result;
+            expr.RightExpr.Accept( this );
+            int right = Result;
+            Result = Compute( left, right, expr.Type );
+        }
+
+        private int Compute( int left, int right, TokenType type )
+        {
+            switch( type )
+            {
+                case TokenType.Plus:
+                    return left + right;
+                case TokenType.Minus:
+                    return left - right;
+                case TokenType.Mult:
+                    return left * right;
+                case TokenType.Div:
+                    return left / right;
+                case TokenType.Modulo:
+                    return left % right;
+                default:
+                    throw new ArgumentException( "Argument is not an operator" );
+            }
         }
 
         public int Result { get; private set; }
