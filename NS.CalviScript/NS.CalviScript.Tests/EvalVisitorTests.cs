beecd76cd1b63247157ce32d4354d5f33571b773
Antoine Raquillet <antoine.raquillet@invenietis.com>
2016-05-24 09:29:31 +0200

    Added evaluation visitor (to implement.)

---
+++ NS.CalviScript/NS.CalviScript.Tests/EvalVisitorTests.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace NS.CalviScript.Tests
+{
+    [TestFixture]
+    public class EvalVisitorTests
+    {
+        [Test]
+        public void can_evaluate_expression()
+        {
+            Tokenizer tokenizer = new Tokenizer( "(3 + 7) / (5 - 2)" );
+            Parser parser = new Parser( tokenizer );
+            IExpr expr = parser.ParseExpression();
+            EvalVisitor sut = new EvalVisitor();
+
+            sut.Visit( expr );
+
+            Assert.That( sut.Result, Is.EqualTo( 3 ) );
+        }
+    }
+}
---
+++ NS.CalviScript/NS.CalviScript/Visitors/EvalVisitor.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NS.CalviScript
+{
+    public class EvalVisitor : IVisitor
+    {
+        public void Visit( ErrorExpr expr )
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Visit( ConstantExpr expr )
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Visit( BinaryExpr expr )
+        {
+            throw new NotImplementedException();
+        }
+
+        public int Result { get; private set; }
+    }
+}
