f1ea53f210c3e8de7bef65b7baa62c2e124de72c
Olivier Spinelli <olivier.spinelli@invenietis.com>
2016-05-26 15:44:43 +0200

    EvalVisitor is now a StandardVisitor.

--- NS.CalviScript/NS.CalviScript/Visitors/EvalVisitor.cs
+++ NS.CalviScript/NS.CalviScript/Visitors/EvalVisitor.cs
@@ -2,56 +2,9 @@
 
 namespace NS.CalviScript
 {
-    public class EvalVisitor : IVisitor<int>
+    public class EvalVisitor : StandardVisitor
     {
-        public int Visit( ErrorExpr expr )
-        {
-            throw new InvalidOperationException( expr.Message );
-        }
 
-        public int Visit( TernaryExpr expr )
-        {
-            return expr.PredicateExpr.Accept( this ) >= 0
-                ? expr.TrueExpr.Accept( this )
-                : expr.FalseExpr.Accept( this );
-        }
 
-        public int Visit( VarDeclExpr expr )
-        {
-            throw new NotImplementedException();
-        }
-
-        public int Visit( BlockExpr expr )
-        {
-            throw new NotImplementedException();
-        }
-
-        public int Visit( LookUpExpr expr )
-        {
-            throw new NotImplementedException();
-        }
-
-        public int Visit( UnaryExpr expr )
-        {
-            return TokenTypeHelpers.Compute( expr.Expr.Accept( this ), expr.Type );
-        }
-
-        public int Visit( AssignExpr expr )
-        {
-            throw new NotImplementedException();
-        }
-
-        public int Visit( ConstantExpr expr )
-        {
-            return expr.Value;
-        }
-
-        public int Visit( BinaryExpr expr )
-        {
-            return TokenTypeHelpers.Compute(
-                expr.LeftExpr.Accept( this ),
-                expr.RightExpr.Accept( this ),
-                expr.Type );
-        }
     }
 }
